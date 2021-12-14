852f8a4916d9f09622eba4be90ef49ee004153e1
MichaelDelcambre <delcambrem@gmail.com>
2021-12-13 23:07:03 -0600

    done by mmd

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return ( <nav> 
+    <a id= "1" href="#home">home</a>
+    <a id= "2" href="#about">about</a>
+    <a id= "3" href="#projects">projects</a>
+    
+  </nav>
+  )
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,51 @@
 import React from "react";
 
+// function ProjectItem({ name, about, technologies }) {
+//   console.log(name)
+//   return (
+//     <div className="project-item">
+//       <h3>{name}</h3>
+//       <p>{about}</p>
+//       <div className="technologies">
+//         {technologies.map(technology =>  {
+//           return(
+//             <span key={technology}>{technology}</span>
+//           )
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
+
+// function ProjectList({ projects }) {
+//   console.log(projects);
+
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">
+//       {projects.map(project => {
+//         return <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>;
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
+
 function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map(technology => {
+          return (
+            <span key={technology}> {technology} </span>)
+        })}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
